b5728ca460ded255434457852e711e9427ef5ee4
D1o0g9s <boodalina@gmail.com>
2016-03-19 17:04:11 -0700

    Added BlackKnightDemo and Button Class

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/BlackKnightDemo.java
@@ -0,0 +1,265 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.ftcrobotcontroller.opmodes.Button;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.exception.RobotCoreException;
+import com.qualcomm.robotcore.hardware.DeviceInterfaceModule;
+import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+
+/**
+ * This program runs a demo TeleOp for the Black Knight
+ *
+ * There are two controllers: one with drive, and the other with everything else -
+ * Main Controller (controller1):
+ * - Drive (Left Joystick)
+ *
+ * Auxiliary Controller (controller2):
+ * - Lift (Right Joystick)
+ * - Launcher + Intake (R Trigger)
+ * - Intake Reverse (R Bumper)
+ * - Goal Lock (L Bumper (up), L Trigger (down))
+ * - Face (X (close), Y (open))
+ *
+ * Created March 19, 2016
+ *
+ *
+ */
+public class BlackKnightDemo extends LinearOpMode
+{
+    DcMotor left_drive;
+    DcMotor right_drive;
+    DcMotor launcher;
+    DcMotor lift;
+    DcMotor intake;
+
+    Servo goal;
+    Servo face;
+    //Motor Powers and Locations
+    public static final float GOAL_CLOSED = 0.0f; // bash this
+    public static final float GOAL_OPEN = 1.0f; // bash this
+    public static final float FACE_CLOSED = 0.0f; // bash this
+    public static final float FACE_OPEN = 1.0f; // bash this
+    public static final float INTAKE_FORWARD_POWER = 0.75f; // bash this (I think this means 75% power?)
+    public static final float INTAKE_REVERSE_POWER = -0.5f; // bash (can we have negative power to reverse?)
+    public static final float LAUNCHER_FORWARD_POWER = 0.90f; // bash
+    public static final float LAUNCHER_REVERSE_POWER = -0.75f; // bash
+
+    //Launcher unjam constants
+    public static final int UNJAM_TOTAL_TIME = 500; // bash time to unjam motor
+    public static final int UNJAM_WAIT = 200; //I believe this is 200ms to cap how long the launcher can be not working
+    public static final int MIN_ENCODER_TICKS = 1; // min number of encoder ticks allowed for the launcher cycle to be counted as OK
+
+    // threshold for joystick
+    public static final float threshold = 0.1f;
+
+    //Running States
+    private static boolean launching = false;
+    private static int intaking = 0; // 0=stopped; -1=running reverse; 1=running forward
+    private static float launcher_dt = 0; //to keep track of launcher time stopped
+
+    //Constructor
+    public BlackKnightDemo(){}
+
+    @Override public void runOpMode() throws InterruptedException {
+        left_drive = hardwareMap.dcMotor.get("left_d");
+        right_drive = hardwareMap.dcMotor.get("right_d");
+        launcher = hardwareMap.dcMotor.get("launcher");
+        lift = hardwareMap.dcMotor.get("lift");
+        intake = hardwareMap.dcMotor.get("intake");
+        goal = hardwareMap.servo.get("goal");
+        face = hardwareMap.servo.get("face");
+        right_drive.setDirection(DcMotor.Direction.REVERSE); // check this in testing
+
+        waitForStart();
+
+        Button controller1 = new Button();
+        Button controller2 = new Button();
+
+
+        ElapsedTime timer = new ElapsedTime();
+        timer.reset(); // to unjam launcher
+
+        float dt;
+        float new_time;
+        float old_time = 0f;
+        float launcherPause = 0f;
+        float unjamTime = 0f;
+
+        float dEncoderTicks;
+        float newNumEncoderTicks;
+        float oldNumEncoderTicks = 0f;
+
+
+        while(true) {
+
+            //============================ Control Map ==========================
+            // drive
+            float[] drive_stick = new float[]{-gamepad1.left_stick_x, -gamepad1.left_stick_y};
+
+            // launcher
+            boolean toggleLauncher = gamepad2.right_trigger>0.0;
+
+            // intake
+            boolean toggleIntake = controller2.press(Button.Buttons.RIGHT_BUMPER);
+
+            // lift
+            float lift_power = Range.clip(gamepad2.right_stick_y, -1, 1);
+
+            // goal
+            boolean goalOpen = controller2.press(Button.Buttons.LEFT_BUMPER);
+            boolean goalClosed = gamepad2.left_trigger>0.0;
+
+            // face
+            boolean faceOpen = controller2.press(Button.Buttons.Y);
+            boolean faceClosed = controller2.press(Button.Buttons.X);
+
+            //================================Drive===============================
+            deadZone(drive_stick);
+            float left_power  = drive_stick[1]-drive_stick[0];
+            float right_power = drive_stick[1]+drive_stick[0];
+
+            right_power = Range.clip(qBezier(right_power), -1, 1);
+            left_power = Range.clip(qBezier(left_power), -1, 1);
+
+            right_drive.setPower(right_power);
+            left_drive.setPower(left_power);
+
+
+            //============================Launcher + Intake========================
+            // Toggles
+            if(toggleLauncher) {
+                launching = !launching;
+            }
+
+            if(toggleIntake) {
+                if(intaking<1) {
+                    intaking++;
+                }
+                else {
+                    intaking = -1;
+                }
+            }
+
+            //timer updater for launcher unjam
+            new_time = (float) timer.time();
+            dt = new_time-old_time;
+            old_time = new_time;
+
+            if(launching) {
+                newNumEncoderTicks = launcher.getCurrentPosition(); //get new value for launcher encoder ticks
+                dEncoderTicks = newNumEncoderTicks - oldNumEncoderTicks;
+                oldNumEncoderTicks = newNumEncoderTicks;
+
+                if(intaking==0) {//if intake is not running and we are launching, run forward
+                    intaking = 1;
+                }
+
+
+                if(dEncoderTicks < MIN_ENCODER_TICKS && dEncoderTicks >= 0) {
+                    launcherPause += dt;
+                }
+                else {
+                    launcherPause = 0;
+                }
+
+                if(launcherPause>=UNJAM_WAIT){
+                    launcherPause = 0;
+                    unjamTime = UNJAM_TOTAL_TIME; //refills time for unjamming
+                }
+
+                if (unjamTime > 0)	{
+                    intaking = 0;
+                    launcher.setPower(LAUNCHER_REVERSE_POWER);
+                    unjamTime -= dt;
+                }
+                else {
+                    launcher.setPower(LAUNCHER_FORWARD_POWER);
+                }
+                //run launcher
+
+            }
+
+            // Run intake based on intaking state
+            if(intaking == 1) {
+                intake.setPower(INTAKE_FORWARD_POWER);
+            }
+            else if(intaking == -1) {
+                intake.setPower(INTAKE_REVERSE_POWER);
+            }
+            else {
+                intake.setPower(0);
+            }
+
+            //================================Lift================================
+            lift.setPower(lift_power);
+
+            //===========================Goal Mechanism==========================
+            if(goalOpen) {
+                goal.setPosition(GOAL_OPEN);
+            }
+            else if(goalClosed) {
+                goal.setPosition(GOAL_CLOSED);
+            }
+
+            //========================Face (Net) Mechanism=======================
+            if (faceOpen) {
+                face.setPosition(FACE_OPEN);
+            }
+            else if(faceClosed) {
+                face.setPosition(FACE_CLOSED);
+            }
+
+            //Refreshes
+            try {
+                controller1.updateButtons(gamepad1.toByteArray());
+                controller2.updateButtons(gamepad2.toByteArray());
+            }
+            catch (RobotCoreException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    //Joystick cleanup functions
+    /**
+     * Applies scalar multiplication to an array of length two.
+     * @param v array to be scaled.
+     * @param scalar scalar which will be multiplied with each element of array.
+     * @return array after scaling.
+     */
+    static float[] scale(float[] v, float scalar) {
+        v[0] *= scalar;
+        v[1] *= scalar;
+        return v;
+    }
+
+
+    /**
+     * Checks if joystick is within threshold of deadzone. Otherwise scales it (?)
+     * @param stick An array for the x and y components of the gamepad joystick.
+     */
+    static void deadZone(float[] stick) {
+        float norm = (float)Math.sqrt(stick[0]*stick[0]+stick[1]*stick[1]);
+        if(norm < threshold) {
+            stick[0] = 0;
+            stick[1] = 0;
+        }
+        else stick = scale(stick, ((norm-threshold)/(1.0f-threshold))/norm);
+    }
+
+    static final float P_0 = 0.0f;
+    static final float P_1 = 10/100.0f;
+    static final float P_2 = 1.0f;
+
+    static float qBezier(float t) {
+        return (t >= 0? 1:-1)*((1-t)*((1-t)*P_0 + t*P_1) + t*((1-t)*P_1 + t*P_2));
+    }
+
+}
